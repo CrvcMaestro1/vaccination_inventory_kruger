2a3726b092159221073104967cee176354c85ad9
Christian Rafael <Crvc1998@gmail.com>
2022-10-17 16:49:02 -0500

    feat: add static url for development purposes

--- vaccination_inventory_kruger/settings.py
+++ vaccination_inventory_kruger/settings.py
@@ -131,3 +131,9 @@ REST_FRAMEWORK = {
         'rest_framework.renderers.JSONRenderer',
     )
 }
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.2/howto/static-files/
+
+STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
