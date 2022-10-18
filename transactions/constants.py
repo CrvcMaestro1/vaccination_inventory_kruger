d06171c8a4d09689f8a9cbed7112ac67037c1de1
Christian Rafael <Crvc1998@gmail.com>
2022-10-17 23:20:54 -0500

    feat: add constants

--- transactions/constants.py
+++ transactions/constants.py
@@ -1,1 +1,7 @@
 EMPLOYEE_PASSWORD_DEFAULT = "The default password is your identification number"
+
+VACCINE_TYPE_IS_MANDATORY = "Vaccine Type is mandatory when the employee is vaccinated"
+
+VACCINATION_DATE_IS_MANDATORY = "Vaccination date is mandatory when the employee is vaccinated"
+
+DOSES_NUMBER_IS_MANDATORY = "Doses number is mandatory when the employee is vaccinated"
